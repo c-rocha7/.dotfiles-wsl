148c241bad7a8f36b06dd1f2a79259630adf33b5
Cauã R. Pereira <7aauac@gmail.com>
2026-09-09 16:00:47 -0300

    Atualiza flake.lock com novas versões de nixpkgs e nixpkgs-unstable; adiciona módulo dev nas configurações do home

--- nix-dotfiles-wsl/modules/home-modules.nix
+++ nix-dotfiles-wsl/modules/home-modules.nix
@@ -3,6 +3,7 @@
 {
   flake.homeModules = {
     /*---- Programs ----*/
+    dev = ./features/programs/_dev.nix;
     nil = ./features/programs/_nil.nix;
     nixd = ./features/programs/_nixd.nix;
     nixpkgs-fmt = ./features/programs/_nixpkgs-fmt.nix;
